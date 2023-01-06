f44e3db97e5d77883c57024ec888e0d04614b06f
smohan67 <siddhu.mohan6@gmail.com>
2023-01-06 17:59:56 -0600

    melon loader support

---
+++ LabLoader.Melon/LabLoaderPlugin.cs
@@ -0,0 +1,13 @@
+﻿using Labloader;
+using MelonLoader;
+
+namespace LabLoader.Melon
+{
+    public class LabLoaderPlugin : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            Loader.Main();
+        }
+    }
+}
